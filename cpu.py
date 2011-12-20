c56a364c627ba0cd8f567248a2d88c9f5c6a45dc
Nicholas Prado <damiengrey@sbcglobal.net>
2011-12-19 23:47:53 -0800

    Now all indirect addressing, maybe too much
    As indicated earlier, I wanted to make addressing consistent
    across instructions. A fetch gets the value pointed to in the address
    of the instruction. It seemed appropriate for a goto to do the same.
    However, in trying to make an ISA program (just the first, ie 12+13)
    I found the indirection too clever by half. I think that this
    implementation is indirecting twice, which is extremely inconvenient.
    
    Nevertheless, I'll sleep on it, look up the spec's definitions for the
    goto/load behavior. Essentially, this commit renders the cpu difficult
    to use and has neither program produces its intended effect. I've only
    uploaded these because it Might be the indirection in the spec and I'm
    just not being careful enough about the isa values.

--- cpu.py
+++ cpu.py
@@ -38,25 +38,31 @@ class Cpu( object ):
 			Cpu.coreDump( self, "Accumulator overflow" )
 		elif -Cpu.word > self.acc:
 			Cpu.coreDump( self, "Accumulator underflow" )
-		# hur hur python, sure I can " -Cpu.word > self.acc > Cpu.word "
-		# but short circuiting means it doesn't check both
+		self.running = False
 	
 	def fetch( self ): # vetted 11 12 7
 		' get next, get indirected value '
 		if self.mem.exceedAddrBound( self.pc ) :
 			Cpu.coreDump( self, "Address out of range" )
+			self.running = False
 		else :
 			self.ir = self.mem.getFrom( self.pc )
-			self.opCode = self.ir / Cpu.halfWord
-			self.opAddr = self.ir % Cpu.halfWord
+			self.opCode = self.ir / Cpu.halfWord # separate high order
+			self.opAddr = self.ir % Cpu.halfWord # separate low order
 			if self.mem.exceedAddrBound( self.opAddr ) :
 				coreDump( "Address out of range" )
+				self.running = False
 			else:
-				self.opVal = self.mem.getFrom( self.opAddr ) # separate low order; also, this is explicitly indirect addressing
+				self.opVal = self.mem.getFrom( self.opAddr )
 				self.pc += 1
-				# um, I hadn't been thinking to hard but I am putting the indirected value on top of opVal
-				# I guess I should put it in a different register
-				# dude, it's not a problem just think of it as another register
+	
+	def validateIndirectAddr( self ) :
+		' Repeating these checks 3 times is ugly '
+		if self.mem.exceedAddrBound( self.opVal ):
+			Cpu.coreDump( self, "Address out of bounds " + str( self.opVal ) )
+			self.running = False
+		else:
+			return True
 	
 	def execute( self ):
 		' elif field of the ISA '
@@ -82,57 +88,41 @@ class Cpu( object ):
 			Cpu.checkOverflow( self )
 		elif Cpu.DIVIDE == self.opCode:
 			print " ratioing %s and %d" % ( self.acc, self.opVal )
-			if self.opVal == 0:
+			if 0 == self.opVal:
 				Cpu.coreDump( self, "Divide by zero? No." )
+				self.running = False
 			else:
 				self.acc /= self.opVal
 				Cpu.checkOverflow( self )
-		# shit all these are getting the next instruction since fetch automatically puts it in there
-		# two options: a) they indirect to the value of the intended address
-		#					 b) these opCodes are direct: they test the address in the IR, not indirected
-		# for the moment, I'll work with B.
-		# in that case, the opAddr was already tested during the fetch, so there's no point in retesting
+		# For ease in initial testing, these all did direct addressing. No longer.
+		# A goto will point to the int value of the line number it actually intends to put in the PC
+		# It may mean a bit more waste at ISA level, but that's what compilers are for.
+		# However, the cpu won't check for bad values if the condition isn't met.
 		elif Cpu.GOTO == self.opCode:
-			print " going to %d" % self.opAddr
-			self.pc = self.opAddr
+			print " going to %d" % self.opVal
+			if Cpu.validateIndirectAddr( ) :
+				self.pc = self.mem.getFrom( self.opVal )
 		elif Cpu.GOTOZERO == self.opCode:
-			print " going to %d if Acc is zero, it is %d" % ( self.opAddr, self.acc )
+			print " ptr %d to %d if Acc is zero, it is %d" % ( self.opVal, self.mem.getFrom( self.opVal ), self.acc )
 			if 0 == self.acc:
-				self.pc = self.opAddr
+				if Cpu.validateIndirectAddr( self ) :
+					self.pc = self.mem.getFrom( self.opVal )
 		elif Cpu.GOTONEG == self.opCode:
-			print " going to %d if Acc is neg, it is %d" % ( self.opAddr, self.acc )
+			print " ptr %d to %d if Acc is neg, it is %d" % ( self.opVal, self.mem.getFrom( self.opVal ), self.acc )
 			if 0 > self.acc:
-				self.pc = self.opAddr
-			'''
-				if self.mem.exceedAddrBound( self.opVal ):
-					Cpu.coreDump( self, "Address out of bounds " + str( self.opVal ) )
-				else:
-					self.pc = self.opVal
-			elif Cpu.GOTOZERO == self.opCode:
-				if 0 == self.acc:
-					if self.mem.exceedAddrBound( self.opVal ):
-						Cpu.coreDump( self, "Address out of bounds " + str( self.opVal ) )
-					else:
-						self.pc = self.opVal
-			elif Cpu.GOTONEG == self.opCode:
-				if 0 > acc:
-					if self.mem.exceedAddrBound( self.opVal ):
-						Cpu.coreDump( self, "Address out of bounds " + str( self.opVal ) )
-					else:
-						self.pc = self.opVal
-			'''
+				if Cpu.validateIndirectAddr( self ) :
+					self.pc = self.mem.getFrom( self.opVal )
 		elif Cpu.LOAD == self.opCode:
-			print " getting from %d" % ( self.opAddr )
-			self.acc = self.mem.getFrom( self.opAddr )
-			# damn it, I'm also going to make this one direct too, at least while the goto's are direct
+			print " getting from %d" % ( self.opVal )
+			if Cpu.validateIndirectAddr( self ) :
+				self.acc = self.mem.getFrom( self.opVal )
 		elif Cpu.STORE == self.opCode:
-			print " saving %s at %d" % ( self.acc, self.opAddr ) # um why does the compiler think the Acc has a String?
-			self.mem.setAt( self.opAddr, self.acc )
-			# shit, more direct addressing. I may have to bite the bullet and indirect everything
-			# I am loath because it is tedious while I am debugging everything
-			# obviously, a compiler can handle itself either way though the wasted memory is ugly too
+			print " saving %s at %d" % ( self.acc, self.opVal )
+			if Cpu.validateIndirectAddr( self ) :
+				self.mem.setAt( self.opVal, self.acc )
 		else :
-			Cpu.coreDump( self, "Unrecognized instruction" )	
+			Cpu.coreDump( self, "Unrecognized instruction" )
+			self.running = False
 			
 	def run( self ):
 		' fetch & execute while running '
@@ -155,7 +145,6 @@ class Cpu( object ):
 		self.mem.coreDump( dump )
 		dump.close( )
 		self.running = False
-		
 		
 		
 		
