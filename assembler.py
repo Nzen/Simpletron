43fc74d2dbc2ad0e867acdddcd33a5c349bb0a90
Nicholas Prado <damiengrey@sbcglobal.net>
2013-01-27 23:46:01 -0800

    Testing done, py2 compliant
    Finished the testing I anticipate at this stage. That's good syntax, and
    checking if it caught errors thrown by bad syntax. I also verified the
    file output. As far as simpletron is concerned, this is it for assembler
    presently.
    
    The other evaluator, however, requires it to be py3 compliant. Luckily, I
    installed it on Cygwin, so I don't need to bother here. I hope it's just
    the print statements, but I'll meet with something unexpected - obviously.
    He probably would also appreciate a better view of the program internals
    as it runs. He will receive the sml codes as output, but that's a bit
    opaque. So, I'll have to either flood this with prints, keep it to select
    areas (cleanup_refs()), or print it like in compiler, by sending generic
    status function an appropriate string.
    
    Of course, SCompiler will need to emit this intermediate assembly instead
    of sml codes. Strictly speaking, I could have it perform either, based on
    input, but that's what master is for. I do admit, though, that none of
    these faculties is portable. Most other vms have registers & various
    addressing modes. I would be working in an underutilizing process if I
    retargeted asm to dcpu10 or mips, and that assembler would need revisions
    to the compiler to handle the larger grammer interface.

--- assembler.py
+++ assembler.py
@@ -7,14 +7,15 @@ hw assessment
 	FINISHED
 initial pass: op var name ;; var val?comm?
 final pass
-
-	DOING
+test driver
 print code
 
+	DOING
+
 	NEED
-test driver
-second pass, resolve forward reference, save file
-(verbose?)
+(verbose for him?)
+(more comments? a ToC?)
+check if literal op addresses exceed length?
 
 raise TypeError('unknown val for var on line ' + str(lines_done))
 '''
@@ -58,7 +59,7 @@ class Assembler( object ) :
 		try :
 			asm_program = open( asm_file ) # py 3 compliant?
 			for line in asm_program :
-				lines_done += 1
+				self.lines_done += 1
 				input = line.rstrip( '\n' )
 				self.initial_pass( input.split( ' ' ) )
 			asm_program.close()
@@ -83,7 +84,7 @@ class Assembler( object ) :
 	def is_a_comment( self, char ) :
 		return char == '#'
 
-	def save_outp( self, op_n, ind_addr ) : # vetted
+	def save_outp( self, op_n, ind_addr ) :
 		'saves pair, side effect: returns index of pair'
 		pair = Instruction( op_n, ind_addr )
 		self.output.append( pair )
@@ -92,15 +93,16 @@ class Assembler( object ) :
 	def to_num_flag( self, in_var ) :
 		'if string is numbers, cast; report whether it did'
 		if self.is_a_comment( in_var[0] ) :
-			raise TypeError('no operand for instruction on line ' + str(lines_done))
+			raise TypeError('no operand for instruction on line ' + str(self.lines_done))
 			# oh, but what about- I was going to say READ, but it's ram direct. hmm
 			# check the spec.
 		elif in_var.isdigit() :
 			return int( in_var ), True
 		else :
+			self.throw_error_if_keyword( in_var )
 			return in_var, False
 
-	def save_var_with_op( self, op_word, ok_var ) : # okay
+	def save_var_with_op( self, op_word, ok_var ) :
 		'if var already saved, put in new Instruction, else save ref_flag'
 		out_index = -1 # init outside of branch, le sigh
 		if ok_var in self.symbols :
@@ -110,16 +112,21 @@ class Assembler( object ) :
 			self.ref_flags.append( out_index )
 		return out_index # in case line has a name
 
-	def save_op_line( self, input, in_op ) : # okay
+	def throw_error_if_keyword( self, var ) :
+		if var in self.asm_sml :
+			raise NameError('keywords can\'t be references, line ' + str(self.lines_done))
+
+	def save_op_line( self, input, in_op ) :
 		ok_var, discard_flag = self.to_num_flag( input[ self.var_spot ] ) # flag irrelevant
 		line_index = self.save_var_with_op( self.asm_sml[ in_op ], ok_var )
 		# handle line name/comment
-		if input.__len__( ) > self.var_spot + 1 : # has another token, maybe line name
+		if input.__len__( ) > self.name_spot : # has another token, maybe line name
 			in_name = input[ self.name_spot ]
 			if not self.is_a_comment( in_name[ 0 ] ) :
+				self.throw_error_if_keyword( in_name )
 				self.symbols[ in_name ] = line_index
 
-	def save_const_line( self, var_value ) : # vetted
+	def save_const_line( self, var_value ) :
 		'line was: "5" ; discard remaining'
 		# this form correct for vars
 		output_index = self.save_outp(  self.asm_sml[ 'no_op' ], var_value )
@@ -132,15 +139,16 @@ class Assembler( object ) :
 		elif maybe_value in self.symbols :
 			return self.output[ self.symbols[maybe_value] ].operand
 		elif not self.is_a_comment( maybe_value[0] ) : # ? var case, ie forward referenced
-			self.ref_flags.append( variable ) # hack to signal s_v_line() to fix with real ind
+			self.throw_error_if_keyword( maybe_value )
+			self.ref_flags.append( maybe_value ) # hack to signal s_v_line() to fix with real ind
 			return 0
 		else : # is a comment
 			return 0
 				
-	def save_variable_line( self, remaining, variable ) : # looks okay
+	def save_variable_line( self, remaining, variable ) :
 		'potentially: x // x 5 // x y ; discard remaining'
 		var_value = 0 # default for variables
-		if len( remaining ) > self.var_spot : # comment or AoT assignment
+		if len( remaining ) > self.var_spot : # comment or assignment
 			var_value = self.s_v_l_determine_value( remaining[ self.var_spot ] )
 		ref_ind = self.save_outp(  self.asm_sml[ 'no_op' ], var_value )
 		self.symbols[ variable ] = ref_ind
@@ -148,7 +156,7 @@ class Assembler( object ) :
 			# newest is the ? var case in s_v_l_d_v()
 			self.ref_flags[ -1 ] = ref_ind
 
-	def save_var_line( self, input, in_var ) : # okay
+	def save_var_line( self, input, in_var ) :
 		var_value, is_num = self.to_num_flag( in_var )
 		if is_num :
 			if var_value not in self.symbols :
@@ -156,9 +164,8 @@ class Assembler( object ) :
 		else :
 			self.save_variable_line( input, in_var )
 
-	#if var found, save mem index; else save symb for searching ref_flags later
 	def initial_pass( self, input ) :
-		'receive either (op var [line name]), var, or #comment'
+		'receive either (op var [line_name]), (var [val|var]), or #comment'
 		first_token = input[ self.op_spot ]
 		if self.is_a_comment( first_token[ 0 ] ) : # whole line
 			return
@@ -174,8 +181,8 @@ class Assembler( object ) :
 			if wanted_symbol in self.symbols :
 				self.output[ problem ].operand = self.symbols[ wanted_symbol ]
 			else :
-				raise ValueError('unreserved val for op on line ' + str(problem) \
-					+ " " + str(wanted_symbol) )
+				raise ValueError('unreserved val "' + str(wanted_symbol) + '" on line ' \
+				+ str(problem) + " : " + str(wanted_symbol) )
 		# do I need to resolve recursive references?
 
 	def become_code( self ) :
@@ -184,7 +191,7 @@ class Assembler( object ) :
 			sml.append( pair.op_type + pair.operand )
 		return sml
 	
-	def index_of_period( file_name ) :
+	def index_of_period( self, file_name ) :
 		for back_ind in range( -1, -6, -1 ) : # 2to3 ? # sort of arbitrary
 			if file_name[ back_ind ] == '.' :
 				return back_ind
@@ -192,9 +199,9 @@ class Assembler( object ) :
 			return -4 # whatever
 
 	def save_sml_file( self, sml_code, original_name ) :
-		file_type = index_of_period( original_name )
+		file_type = self.index_of_period( original_name )
 		new_name = original_name[ : file_type ] + '.asm' # hopefully
-		output = open( newFileName, 'w' )
+		output = open( new_name, 'w' )
 		output.truncate( ) # erase what's in there for safety
 		for nn in sml_code :
 			output.write( str( nn ) + '\n' )
@@ -223,24 +230,53 @@ def test_good_syn( simple ) :
 			worked = False
 	if worked :
 		print "good syntax vetted, for now"
+	else :
+		print "what?"
+
+def t_b_s_catch( simple, bad_asm, the_error ) :
+	'better with a testing module, of course'
+	try :
+		became = simple.assemble_stream( bad_asm )
+		print 'worked, so it\'s a bad test ' + str(simple.output[0])
+		return False
+	except the_error :
+		return True
+	except Exception as unexpected : # very 2to3
+		import traceback
+		print "Caught \"" + type(unexpected).__name__ + " - " + str(unexpected) + \
+		"\" instead of " + str(the_error) + " in \"" + bad_asm[0] +"\""
+		return False
 
 def test_bad_syn( simple ) :
-	'''
-	x x # hmm?
-	HALT #unary unfortunately
-	TIMES not_init
-	y TIMES #maybe?
-	'''
-	print "doesnt test wrong syntax" # 2to3
+	errors_caught = []
+	errors_caught.append( t_b_s_catch( simple, ['HALT #actually unary'], TypeError ) )
+	simple.t_clear()
+	errors_caught.append( t_b_s_catch( simple, ['TIMES not_init'], ValueError ) ) # actually is 0
+	simple.t_clear()
+	errors_caught.append( t_b_s_catch( simple, ['x x'], ValueError ) ) # hmm that works...
+	simple.t_clear()
+	errors_caught.append( t_b_s_catch( simple, ['ADD_ 2 TIMES'], NameError ) ) # not raising
+	simple.t_clear()
+	errors_caught.append( t_b_s_catch( simple, ['y TIMES'], NameError ) )
+	#errors_caught.append( t_b_s_catch( simple, ['bad'], x_Error ) )
+	if False in errors_caught :
+		print "--Didn't catch a syntactic error" # 2to3
+	else :
+		print "bad syntax caught"
 
 def test_syntax( simple ) :
 	test_bad_syn( simple )
 	simple.t_clear()
 	test_good_syn( simple )
+	simple.t_clear()
+
+def test_with_file( simple ) :
+	print "file printed to " + simple.assemble_file( "asmMonkey.txt" ) # 2to3
 
 def test_assembler() :
 	simple = Assembler()
 	test_syntax(simple)
+	test_with_file( simple )
 	
 test_assembler()
 
